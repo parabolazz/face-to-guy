e2e9b24eae09cbf9e3101e4c5c85367b1c0489dd
陈竹之 <parebolayh@outlook.com>
2023-05-11 23:43:56 +0800

    feat: 持续迭代匹配用户页面

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,9 +1,30 @@
 <template>
   <div class="matched-user">
     <div class="matched-user__profile">
-      <div class="matched-user__profile__photos">
+      <div class="matched-user__profile-photos">
         <div class="matched-user__profile__photos-mask top"></div>
-
+        <div class="matched-user__profile__photos-name absolute">
+          {{ profile.name }}
+        </div>
+        <div class="matched-user__profile__photos-info">
+          <div class="flex">
+            <div class="matched-user__profile__photos-info-item">
+              {{ profile.type }} /
+            </div>
+            <div class="matched-user__profile__photos-info-item">
+              {{ profile.height }}cm /
+            </div>
+            <div class="matched-user__profile__photos-info-item">
+              {{ profile.weight }}kg /
+            </div>
+            <div class="matched-user__profile__photos-info-item">
+              {{ profile.bodyType }}
+            </div>
+          </div>
+          <div class="matched-user__profile__photos-info-ip">
+            IP 归属地: {{ profile.ipSite }}
+          </div>
+        </div>
         <nut-swiper
           :pagination-visible="true"
           :auto-play="0"
@@ -24,6 +45,9 @@
           </nut-swiper-item>
         </nut-swiper>
         <div class="matched-user__profile__photos-mask bottom"></div>
+      </div>
+      <div class="matched-user__profile-intro">
+        {{ profile.intro }}
       </div>
     </div>
     <div class="matched-user__footer">
@@ -53,16 +77,25 @@ const profile = reactive({
     'https://media.vogue.com.tw/photos/5e65e6702b0ea500085cc6b0/2:3/w_2560%2Cc_limit/IMG_9039.JPG',
     'https://pic2.zhimg.com/80/v2-440034de16c1e3fb1527cb33b4b4f049_1440w.webp',
   ],
+  name: 'Forever 纯1???',
+  type: 'Tp',
+  height: 188,
+  weight: 88,
+  bodyType: '肌肉',
+  ipSite: '深圳',
+  intro:
+    '国語、数学、英語、歴史...何でもSimejiAI先生に聞いてみてください！一緒に楽しく勉強しましょう！',
 });
 </script>
 
 <style lang="scss">
 .matched-user {
   .matched-user__profile {
-    padding: 14px;
+    padding: 0 14px;
     overflow-x: hidden;
-    position: relative;
-
+    .matched-user__profile-photos {
+      position: relative;
+    }
     .matched-user__profile__swiper {
       overflow-x: hidden;
       border-radius: 10px;
@@ -90,6 +123,33 @@ const profile = reactive({
         background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 100%);
       }
     }
+    .matched-user__profile__photos-name {
+      position: absolute;
+      top: 14px;
+      left: 14px;
+      font-size: 24px;
+      z-index: 20;
+      color: #fff;
+      font-weight: bold;
+    }
+    .matched-user__profile__photos-info {
+      position: absolute;
+      bottom: 30px;
+      left: 14px;
+      z-index: 20;
+      color: #fff;
+      .matched-user__profile__photos-info-item {
+        font-size: 18px;
+      }
+      .matched-user__profile__photos-info-ip {
+        font-size: 14px;
+        color: rgb(128, 128, 128);
+      }
+    }
+  }
+  .matched-user__profile-intro {
+    font-size: 14px;
+    color: #fff;
   }
   .matched-user__footer {
     position: fixed;
